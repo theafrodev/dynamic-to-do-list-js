8502ecd60ba550bde1708cfa0abdc81e2085670d
theafrodev <fialorchristian@gmail.com>
2024-07-21 10:19:03 +0000

    local storage

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 //Run script after everything loads
 
 document.addEventListener('DOMContentLoaded', () => {
+
     const addButton = document.getElementById("add-task-btn");
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
 
-    function addTask(){
-        let taskText = taskInput.value.trim();
-        if(taskText == ""){
-            alert("Enter a task");
+    loadTasks();
+
+    function addTask(taskText = '', save = true){
+
+        if (taskText === '') {
+            taskText = taskInput.value.trim();
+        }
+
+        if(taskText === ""){
+            if (save){
+                alert("Enter a task");
+            }
         }else{
             //Add and remove tasks
             let taskListItem = document.createElement('li');
@@ -22,12 +31,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
             taskList.appendChild(taskListItem);
 
-            taskText = "";
+            // taskText = "";
 
-            taskInput.value= taskText;
+            taskInput.value = '';
 
             console.log(taskList);
 
+            if (save) {
+                const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+                storedTasks.push(taskText);
+                localStorage.setItem('tasks', JSON.stringify(storedTasks));
+            }
+
+            //Remove tasks
             const removeButtons = document.querySelectorAll('.remove-btn');
 
             removeButtons.forEach(button => {
@@ -35,9 +51,15 @@ document.addEventListener('DOMContentLoaded', () => {
                     const parent = this.closest('li');
                     if (parent) {
                         parent.remove();
+
+                        // Update localStorage
+                        const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+                        const updatedTasks = storedTasks.filter(task => task !== taskText);
+                        localStorage.setItem('tasks', JSON.stringify(updatedTasks));
                     }
                 });
             });
+
         }
     }
 
@@ -51,5 +73,10 @@ document.addEventListener('DOMContentLoaded', () => {
             addTask();
         }
     });
+
+    function loadTasks(){
+        const savedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+        savedTasks.forEach(taskText => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
+    }
     
 });
